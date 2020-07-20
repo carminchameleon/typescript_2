0f24316ba7c0f738683f37cf21a78b0edc836768
eunji hwang <hwanggee1211@gmail.com>
2020-07-20 18:28:36 +0900

    try

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log(123);
+var logSomthing = function () {
+    console.log("something");
+};
+logSomthing();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log(123);
+
+const logSomthing = () => {
+  console.log("something");
+};
+
+logSomthing();
